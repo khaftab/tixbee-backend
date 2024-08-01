caee4fdc6c86ae3dfe5c81489d06e933c362ce7e
khaftab <kh7aftab8uddin6ahmed@gmail.com>
2024-08-01 12:30:13 +0530

    setup dockerfile for next js client - add deployment & service config file - add hot reloading to client

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -20,3 +20,4 @@ start();
 
 // We are setting jwt secret key in k8s secret objects. We don't list them in config file rather directly paste them in the cluster. So, that we don't expose the key in config files.
 // kubectl create secret generic jwt-secret --from-literal JWT_KEY=dd37cf85ecf33cc4
+// kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.11.1/deploy/static/provider/cloud/deploy.yaml
--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // except for webpack, other parts are left as generated
+  webpack: (config, context) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300
+    }
+    return config
+  }
+}
+export default nextConfig
 
-export default nextConfig;
+// This configuration will enable polling in development mode, which will make the file system check for changes every second. https://github.com/vercel/next.js/issues/36774#issuecomment-1211818610
