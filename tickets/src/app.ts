33b42e3655b25e74faa0c1a40c288c42f96fa983
khaftab <kh7aftab8uddin6ahmed@gmail.com>
2024-12-30 19:48:56 +0530

    rewrite dockerfile to use multi stage build - add origin env

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -10,12 +10,12 @@ const app = express();
 // enable cors
 app.use(
   cors({
-    origin: ["https://localhost:3200", "http://localhost:5173"],
+    origin: JSON.parse(process.env.ORIGIN_URL || "[]"),
     credentials: true,
   })
 );
 app.set("trust proxy", true);
-app.use(express.json({ limit: "10mb" })); // Increase JSON payload limit
+app.use(express.json({ limit: "10mb" })); // Increase JSON payload limit for large image uploads (base64 encoded) in wysiwyg editor.
 app.use(
   cookieSession({
     signed: false, // Disable encryption on cookie because JWT is already encrypted. And other services need to read the cookie as well where they might not know decryption algorithm.
--- tickets/src/server.ts
+++ tickets/src/server.ts
@@ -4,23 +4,15 @@ import { natsWrapper } from "./nats-wrapper";
 import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
 import { PaymentCreatedListener } from "./events/listeners/payment-created-listener";
-import { logger } from "@kh-micro-srv/common";
+import { checkEnvVariables, logger } from "@kh-micro-srv/common";
 
 const start = async () => {
-  if (!process.env.JWT_KEY) {
-    throw new Error("JWT_KEY must be defined for tickets");
-  }
-  if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI must be defined for tickets");
-  }
-  if (!process.env.NATS_URL) {
-    throw new Error("NATS_URL must be defined for tickets");
-  }
+  checkEnvVariables(["JWT_KEY", "NATS_URL", "SERVICE_NAME", "TIXBEE_SOURCE_TOKEN", "ORIGIN_URL"]);
   try {
-    await natsWrapper.connect(process.env.NATS_URL);
+    await natsWrapper.connect(process.env.NATS_URL!);
     natsWrapper.client.closed().then(() => {
       logger.info("NATS connection closed.");
-      process.exit(0); // will exit the node server when nats connection is closed.
+      process.exit(0);
     });
     process.on("SIGINT", natsWrapper.shutdown);
     process.on("SIGTERM", natsWrapper.shutdown);
@@ -29,7 +21,7 @@ const start = async () => {
     new OrderCancelledListener(natsWrapper.client).listen();
     new PaymentCreatedListener(natsWrapper.client).listen();
 
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI!);
     logger.info("Connected to MongoDB");
     app.listen(3000, () => {
       logger.info("Tickets service running on port 3000");
